33ef67428944312603960c11ba0612b591562de7
Emergency-Paperclip <ejnelson1997@gmail.com>
2020-05-16 21:08:30 -0600

    added the stuff

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is Savannah");
+        System.out.println("Hello Savannah, my name is Ethan");
 
     }
 }
